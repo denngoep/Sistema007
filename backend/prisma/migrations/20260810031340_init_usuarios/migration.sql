6f8e28be8fca8026dba8907a6e04dced307f7678
denngoep <degonzalez376@gmail.com>
2026-08-09 22:23:11 -0500

    feat: configurar base de datos MySQL con Prisma HU-004

---
+++ backend/prisma/migrations/20260810031340_init_usuarios/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `usuarios` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `correo` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `activo` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `usuarios_correo_key`(`correo`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
